c74af0ed4f2dda7337fbd89344068d35b0e4b4d9
Ailton Sena <ailtonsenap@gmail.com>
2021-05-28 20:43:40 -0300

    fix: config

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     entry:'./src/index.tsx',
     output: {
         path: path.join(__dirname, 'public/js/'),
-        publicPath: '/public/js/',
+        publicPath: '/js/',
         filename: 'bundle.js'
     },
     resolve: {
